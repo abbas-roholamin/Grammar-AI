52ed4ef7aba29328a894b2182d2b5e7e3028d488
Ali Reza <webdeve1083@gmail.com>
2023-03-02 15:51:05 +0800

    Added a navbar, and some reasons

---
+++ content/Information.ts
@@ -0,0 +1,38 @@
+export const WHY_GRAMMAR_AI = [
+  {
+    reason: 1,
+    title: "Improving writing skills",
+    details:
+      "By using a grammar AI website, users can improve their writing skills by learning from the suggestions and corrections provided by the tool. This can help them become better writers over time.",
+  },
+  {
+    reason: 2,
+    title: "Saving time",
+    details:
+      "Checking grammar and spelling manually can be time-consuming, especially for longer documents. A grammar AI website can quickly scan and identify errors, saving users time and effort.",
+  },
+  {
+    reason: 3,
+    title: "Professionalism",
+    details:
+      "Good grammar and spelling are important for professional communication. Using a grammar AI website can help ensure that emails, reports, and other documents are error-free and appear professional.",
+  },
+  {
+    reason: 4,
+    title: "Convenience",
+    details:
+      "A grammar AI website can be accessed from anywhere with an internet connection, making it a convenient tool for people who frequently write on the go.",
+  },
+  {
+    reason: 5,
+    title: "Accuracy",
+    details:
+      "AI websites use advanced algorithms to identify errors, which can be more accurate than relying on human proofreading alone.",
+  },
+  {
+    reason: 6,
+    title: "Accessibility",
+    details:
+      "For people with disabilities or language barriers, using a grammar AI website can help them communicate more effectively by identifying and correcting errors that may be difficult to spot otherwise.",
+  },
+];
--- pages/index.tsx
+++ pages/index.tsx
@@ -12,13 +12,14 @@ const inter = Inter({
 });
 
 import { MdOutlineContentCopy } from "react-icons/md";
+import { WHY_GRAMMAR_AI } from "@/content/Information";
 
 export default function Home() {
   const [promptValue, setPromptValue] = useState("");
   const [generated, setGenerated] = useState("");
   const [loader, setLoader] = useState("Generate");
 
-  const prompt = `I will give a sentence, please correct it or rewrite it, the sentence is: "${promptValue}"`;
+  const prompt = `I will give you a sentence, Please correct it or rewrite it to a professional manner, the following sentence is: "${promptValue}"`;
 
   const generating = async () => {
     setLoader("Generating...");
@@ -34,7 +35,7 @@ export default function Home() {
 
     setGenerated(data.choices[0].text);
     confetti();
-    setLoader("Generate");
+    setLoader("Generate Again");
   };
 
   const copingText = (value: string) => {
@@ -53,20 +54,37 @@ export default function Home() {
       <main
         className={`${inter.className} flex flex-col items-center w-full max-w-[1200px] mx-auto p-4`}
       >
-        <h1 className="font-black text-6xl text-center mt-24 mb-4">
+        <nav className="fixed top-0 left-0 z-50 w-full px-4 bg-white shadow-md">
+          <div className="w-full max-w-[1200px] mx-auto h-16 flex justify-between items-center">
+            <h1 className="text-xl font-black">Grammar AI</h1>
+            <a
+              href="https://www.alirezasamadi.com/"
+              target={"_blank"}
+              className="flex items-center gap-4 text-sm"
+            >
+              Developed by
+              <img
+                src="https://www.alirezasamadi.com/_next/image?url=%2F_next%2Fstatic%2Fmedia%2FMy%20Image.0e022541.png&w=256&q=75"
+                alt=""
+                className="w-10 h-10 rounded-full"
+              />
+            </a>
+          </div>
+        </nav>
+        <h1 className="mt-24 mb-4 text-4xl font-black text-center md:text-6xl">
           Grammar AI
         </h1>
-        <p className="mb-24 text-base md:text-xl font-medium opacity-75 text-center">
+        <p className="mb-24 text-base font-medium text-center opacity-75 md:text-xl">
           This application makes it easy for users to improve their writing and
           avoid common mistakes.
         </p>
         <textarea
-          className="border-2 outline-none border-gray-500 focus:border-black rounded-lg p-4 w-full md:w-4/5 lg:w-1/2"
+          className="w-full p-4 border-2 border-gray-500 rounded-lg outline-none focus:border-black md:w-4/5 lg:w-1/2"
           value={promptValue}
           onChange={(e) => setPromptValue(e.target.value)}
           placeholder="e.g: I okay am"
         />
-        <p className="w-full md:w-4/5 lg:w-1/2 text-sm opacity-70 mt-1">
+        <p className="w-full mt-1 text-sm md:w-4/5 lg:w-1/2 opacity-70">
           It is either going to rewrite it or correct it.
         </p>
         <button
@@ -79,31 +97,35 @@ export default function Home() {
         </button>
 
         {generated.length !== 0 && (
-          <div className="w-full h-screen fixed top-0 left-0 bg-white bg-opacity-80 flex justify-center items-center">
+          <div className="fixed top-0 left-0 z-40 flex items-center justify-center w-full h-screen isolate">
+            <div
+              onClick={() => setGenerated("")}
+              className="absolute top-0 left-0 w-full h-full bg-white bg-opacity-80 -z-10"
+            ></div>
             <div className="bg-white w-full max-w-[800px] shadow-2xl p-4 rounded-lg overflow-hidden border-2 border-gray-200">
-              <h2 className="font-medium text-2xl my-4 mt-6">Your Sentence</h2>
+              <h2 className="my-4 mt-6 text-2xl font-medium">Your Sentence</h2>
               <motion.p
                 initial={{ opacity: 0, scale: 0 }}
                 animate={{ opacity: 1, scale: 1 }}
                 transition={{ duration: 0.4 }}
-                className="bg-slate-200 p-4 rounded-md"
+                className="p-4 rounded-md bg-slate-200"
               >
                 {promptValue}
               </motion.p>
-              <h2 className="font-medium text-2xl my-4 mt-6">
+              <h2 className="my-4 mt-6 text-2xl font-medium">
                 Generated Sentence
               </h2>
               <motion.p
                 initial={{ opacity: 0, scale: 0 }}
                 animate={{ opacity: 1, scale: 1 }}
                 transition={{ duration: 0.4, delay: 0.1 }}
-                className="bg-black text-white p-4 rounded-md"
+                className="p-4 text-white bg-black rounded-md"
               >
                 {generated}
               </motion.p>
               <button
                 onClick={() => copingText(generated)}
-                className="bg-black text-white p-4 rounded-md mt-8 focus:bg-green-700 duration-100"
+                className="p-4 mt-8 text-white duration-100 bg-black rounded-md focus:bg-green-700"
               >
                 <MdOutlineContentCopy />
               </button>
@@ -111,6 +133,26 @@ export default function Home() {
             </div>
           </div>
         )}
+
+        <div className="w-full pb-12">
+          <h1 className="mt-24 mb-8 text-3xl font-black md:text-4xl">
+            Why Grammar AI?
+          </h1>
+          <div className="flex flex-wrap gap-4">
+            {WHY_GRAMMAR_AI.map((reasons) => (
+              <div
+                key={reasons.reason}
+                className="basis-[300px] grow bg-gray-100 p-4 rounded-lg group hover:bg-gray-200"
+              >
+                <small className="flex items-center justify-center w-8 h-8 font-bold text-white duration-200 bg-gray-800 rounded-full group-hover:bg-black">
+                  {reasons.reason}
+                </small>
+                <h2 className="my-2 text-xl font-bold">{reasons.title}</h2>
+                <p className="opacity-90">{reasons.details}</p>
+              </div>
+            ))}
+          </div>
+        </div>
       </main>
     </>
   );

0bbb7b4bc579822b1ecbbafaa14ba0ebc80ca187
Ali Reza <webdeve1083@gmail.com>
2023-03-02 13:58:16 +0800

    Almost done with the Grammar AI

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,13 +1,38 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
+import type { NextApiRequest, NextApiResponse } from "next";
 
 type Data = {
-  name: string
-}
+  name: string;
+};
 
-export default function handler(
+export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ name: 'John Doe' })
+  const { prompt } = req.body;
+
+  console.log(prompt);
+
+  const payload = {
+    model: "text-davinci-003",
+    prompt,
+    max_tokens: 200,
+    temperature: 0.7,
+    top_p: 1,
+    n: 1,
+    stream: false,
+  };
+
+  const response = await fetch("https://api.openai.com/v1/completions", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${process.env.NEXT_PUBLIC_APIKEY}`,
+    },
+    body: JSON.stringify(payload),
+  });
+
+  const json = await response.json();
+
+  res.status(200).send(json);
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,47 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import styles from '@/styles/Home.module.css'
+import Head from "next/head";
+import { useState } from "react";
+import { Inter } from "next/font/google";
+import { motion } from "framer-motion";
+import confetti from "canvas-confetti";
+import toast, { Toaster } from "react-hot-toast";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({
+  weight: ["400", "900", "600", "500"],
+  display: "swap",
+  subsets: ["latin"],
+});
+
+import { MdOutlineContentCopy } from "react-icons/md";
 
 export default function Home() {
+  const [promptValue, setPromptValue] = useState("");
+  const [generated, setGenerated] = useState("");
+  const [loader, setLoader] = useState("Generate");
+
+  const prompt = `I will give a sentence, please correct it or rewrite it, the sentence is: "${promptValue}"`;
+
+  const generating = async () => {
+    setLoader("Generating...");
+    const res = await fetch("/api/hello", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ prompt }),
+    });
+
+    const data = await res.json();
+
+    setGenerated(data.choices[0].text);
+    confetti();
+    setLoader("Generate");
+  };
+
+  const copingText = (value: string) => {
+    navigator.clipboard.writeText(value);
+    toast("✅ Copied");
+  };
+
   return (
     <>
       <Head>
@@ -14,110 +50,68 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={styles.main}>
-        <div className={styles.description}>
-          <p>
-            Get started by editing&nbsp;
-            <code className={styles.code}>pages/index.tsx</code>
-          </p>
-          <div>
-            <a
-              href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              By{' '}
-              <Image
-                src="/vercel.svg"
-                alt="Vercel Logo"
-                className={styles.vercelLogo}
-                width={100}
-                height={24}
-                priority
-              />
-            </a>
+      <main
+        className={`${inter.className} flex flex-col items-center w-full max-w-[1200px] mx-auto`}
+      >
+        <h1 className="font-black text-6xl text-center mt-24 mb-4">
+          Grammar AI
+        </h1>
+        <p className="mb-24 text-xl font-medium opacity-75">
+          This application makes it easy for users to improve their writing and
+          avoid common mistakes.
+        </p>
+        <textarea
+          className="border-2 outline-none border-gray-500 focus:border-black rounded-lg p-4 w-1/2"
+          value={promptValue}
+          onChange={(e) => setPromptValue(e.target.value)}
+          placeholder="e.g: I okay am"
+        />
+        <p className="w-1/2 text-sm opacity-70 mt-1">
+          It is either going to rewrite it or correct it.
+        </p>
+        <button
+          onClick={generating}
+          className={`bg-black text-white w-1/2 rounded-lg py-2 my-5 ${
+            loader == "Generating..." && "animate-pulse"
+          }`}
+        >
+          {loader}
+        </button>
+
+        {generated.length !== 0 && (
+          <div className="w-full h-screen fixed top-0 left-0 bg-white bg-opacity-80 flex justify-center items-center">
+            <div className="bg-white w-full max-w-[800px] shadow-2xl p-4 rounded-lg overflow-hidden border-2 border-gray-200">
+              <h2 className="font-medium text-2xl my-4 mt-6">Your Sentence</h2>
+              <motion.p
+                initial={{ opacity: 0, scale: 0 }}
+                animate={{ opacity: 1, scale: 1 }}
+                transition={{ duration: 0.4 }}
+                className="bg-slate-200 p-4 rounded-md"
+              >
+                {promptValue}
+              </motion.p>
+              <h2 className="font-medium text-2xl my-4 mt-6">
+                Generated Sentence
+              </h2>
+              <motion.p
+                initial={{ opacity: 0, scale: 0 }}
+                animate={{ opacity: 1, scale: 1 }}
+                transition={{ duration: 0.4, delay: 0.1 }}
+                className="bg-black text-white p-4 rounded-md"
+              >
+                {generated}
+              </motion.p>
+              <button
+                onClick={() => copingText(generated)}
+                className="bg-black text-white p-4 rounded-md mt-8 focus:bg-green-700 duration-100"
+              >
+                <MdOutlineContentCopy />
+              </button>
+              <Toaster />
+            </div>
           </div>
-        </div>
-
-        <div className={styles.center}>
-          <Image
-            className={styles.logo}
-            src="/next.svg"
-            alt="Next.js Logo"
-            width={180}
-            height={37}
-            priority
-          />
-          <div className={styles.thirteen}>
-            <Image
-              src="/thirteen.svg"
-              alt="13"
-              width={40}
-              height={31}
-              priority
-            />
-          </div>
-        </div>
-
-        <div className={styles.grid}>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Docs <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Find in-depth information about Next.js features and&nbsp;API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Learn <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Learn about Next.js in an interactive course with&nbsp;quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Templates <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Discover and deploy boilerplate example Next.js&nbsp;projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Deploy <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Instantly deploy your Next.js site to a shareable URL
-              with&nbsp;Vercel.
-            </p>
-          </a>
-        </div>
+        )}
       </main>
     </>
-  )
+  );
 }
